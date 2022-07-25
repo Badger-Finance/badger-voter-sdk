0cb2b1ea97da21d56bec50f0a72eabd71fba7b0d
SHAKOTN <jadecoresky@gmail.com>
2022-07-25 10:54:19 +0300

    chore: add license and setup files

---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt", "r") as f:
+    requirements = list(map(str.strip, f.read().split("\n")))[:-1]
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="badger-voter-sdk",
+    install_requires=requirements,
+    author="SHAKOTN",
+    author_email="andrii@badger.com",
+    description="Shared code for badger autovoters",
+    long_description=README,
+    keywords=["badger-voter-sdk"],
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=False,
+    version="0.2.5",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    license="MIT",
+    url="https://github.com/Badger-Finance/badger-voter-sdk",
+    python_requires=">=3.7,<4",
+)
