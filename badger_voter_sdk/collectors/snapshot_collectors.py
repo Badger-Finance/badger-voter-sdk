59fbbfddc6b4dd8b6488644d76093fd6cc27942d
SHAKOTN <jadecoresky@gmail.com>
2022-08-24 18:44:47 +0300

    feat: add gauge weight snapshot fetching by space

--- badger_voter_sdk/collectors/snapshot_collectors.py
+++ badger_voter_sdk/collectors/snapshot_collectors.py
@@ -1,10 +1,15 @@
+import re
 from typing import Dict
 from typing import Optional
 
 from gql import gql
+from web3 import Web3
 
 from badger_voter_sdk.collectors.transports import make_gql_client
 from badger_voter_sdk.constants import SNAPSHOT_GQL_API_URL
+
+SNAPSHOT_MIN_AMOUNT_POOLS = 10
+SNAPSHOT_STATE_ACTIVE = "active"
 
 GET_VOTES_Q = ("""
     query Votes {{
@@ -60,6 +65,43 @@ query {{
 """)
 
 
+GET_ACTIVE_PROPOSALS_Q = lambda first, skip, space: gql(f"""
+query {{
+  proposals (
+    first: {first},
+    skip: {skip},
+    where: {{
+      space_in: ["{space}"],
+      state: "active"
+      network_in: ["1"]
+    }},
+    orderBy: "created",
+    orderDirection: asc
+  ) {{
+    id
+    title
+    body
+    start
+    end
+    snapshot
+    strategies {{
+      name
+      network
+      params
+    }}
+    choices
+    network
+    state
+    author
+    space {{
+      id
+      name
+    }}
+  }}
+}}
+""")
+
+
 def get_voters(snapshot_id: str) -> Optional[Dict]:
     """
     Get all voters on given snapshot
@@ -93,3 +135,37 @@ def get_snapshot_by_id(snapshot_id: str) -> Optional[Dict]:
         if proposal['id'] == snapshot_id:
             target_snapshot = proposal
     return target_snapshot
+
+
+def get_gauge_weight_snapshot(web3: Web3, space: str) -> Optional[Dict]:
+    """
+    Using title re match and some pool heuristics, tries to get current active
+    gauge voting proposal. If not found, returns None
+    :params:
+        - web3: Web3 instance with valid node url
+        - space: snapshot voting space, like aurafinance.eth
+    """
+    client = make_gql_client(SNAPSHOT_GQL_API_URL)
+    limit = 100
+    offset = 0
+    while True:
+        result = client.execute(GET_ACTIVE_PROPOSALS_Q(first=limit, skip=offset, space=space))
+        offset += limit
+        if not result or not result.get("proposals"):
+            break
+        gauge_proposal = None
+        for proposal in result['proposals']:
+            if proposal['state'] != SNAPSHOT_STATE_ACTIVE:
+                continue
+            match = re.match(r"Gauge Weight for Week of .+", proposal['title'])
+            number_of_choices = len(proposal['choices'])
+            current_timestamp = web3.eth.getBlock(web3.eth.get_block_number())['timestamp']
+            # Use heuristics to find out the latest gauge proposal since there is no other way
+            # to filter out proposals
+            if match and number_of_choices > SNAPSHOT_MIN_AMOUNT_POOLS:
+                # Sanity check: proposal should have been started before current date and
+                # should end after
+                if proposal['end'] > current_timestamp > proposal['start']:
+                    gauge_proposal = proposal
+                    break
+        return gauge_proposal
--- tests/test_collectors/test_snapshot_collectors.py
+++ tests/test_collectors/test_snapshot_collectors.py
@@ -1,10 +1,14 @@
+from copy import deepcopy
 from unittest.mock import MagicMock
 
+import pytest
+
+from badger_voter_sdk.collectors.snapshot_collectors import get_gauge_weight_snapshot
 from badger_voter_sdk.collectors.snapshot_collectors import get_snapshot_by_id
 from badger_voter_sdk.collectors.snapshot_collectors import get_voters
+from tests.test_data.snapshot_data import ACTIVE_PROPOSAL_DATA
 from tests.test_data.snapshot_data import SNAPSHOT_DATA_BY_ID
 from tests.test_data.snapshot_data import STALE_VOTES_SNAPSHOT
-
 
 TESTED_VOTER_ADDRESS = "0x14F83fF95D4Ec5E8812DDf42DA1232b0ba1015e6"
 
@@ -71,3 +75,78 @@ def test_get_snapshot_by_id_empty_res(mocker):
     assert not get_snapshot_by_id(
         "0x632726577e9c186a8827311b8702dcfd98f26376237b9715e64eafa61d6d7672"
     )
+
+
+def test_get_gauge_weight_snapshot_happy(mocker):
+    mocker.patch(
+        'badger_voter_sdk.collectors.snapshot_collectors.make_gql_client',
+        return_value=MagicMock(
+            execute=MagicMock(
+                return_value=ACTIVE_PROPOSAL_DATA,
+            )
+        )
+    )
+    web3 = MagicMock(eth=MagicMock(
+        getBlock=MagicMock(
+            return_value={'timestamp': 1658390415}
+        )
+    ))
+    result = get_gauge_weight_snapshot(web3, space='aurafinance.eth')
+    assert result['title'] == "Gauge Weight for Week of 21st July 2022"
+    assert result['state'] == "active"
+    assert result['space'] == {'id': 'aurafinance.eth', 'name': 'Aura Finance'}
+    assert result['strategies'] is not None
+
+
+def test_get_gauge_weight_snapshot_empty_res(mocker):
+    mocker.patch(
+        'badger_voter_sdk.collectors.snapshot_collectors.make_gql_client',
+        return_value=MagicMock(
+            execute=MagicMock(
+                return_value={},
+            )
+        )
+    )
+    assert not get_gauge_weight_snapshot(web3=MagicMock(), space="aurafinance.eth")
+
+
+@pytest.mark.parametrize('timestamp', [1648684815, 2948684815])
+def test_get_gauge_weight_snapshot_no_match(mocker, timestamp):
+    """
+    timestamp is way off from proposal start and end times
+    """
+    mocker.patch(
+        'badger_voter_sdk.collectors.snapshot_collectors.make_gql_client',
+        return_value=MagicMock(
+            execute=MagicMock(
+                return_value=ACTIVE_PROPOSAL_DATA,
+            )
+        )
+    )
+    web3 = MagicMock(eth=MagicMock(
+        getBlock=MagicMock(
+            return_value={'timestamp': timestamp}
+        )
+    ))
+
+    assert not get_gauge_weight_snapshot(web3=web3, space="aurafinance.eth")
+
+
+def test_get_gauge_weight_snapshot_no_active(mocker):
+    data = deepcopy(ACTIVE_PROPOSAL_DATA)
+    data['proposals'][0]['state'] = "closed"
+    mocker.patch(
+        'badger_voter_sdk.collectors.snapshot_collectors.make_gql_client',
+        return_value=MagicMock(
+            execute=MagicMock(
+                return_value=data,
+            )
+        )
+    )
+    web3 = MagicMock(eth=MagicMock(
+        getBlock=MagicMock(
+            return_value={'timestamp': 1657159210}
+        )
+    ))
+
+    assert not get_gauge_weight_snapshot(web3=web3, space="aurafinance.eth")
