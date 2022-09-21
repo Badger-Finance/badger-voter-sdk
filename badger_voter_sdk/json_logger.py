346a15133893793ba2580b98e0d9f3ebd93662a8
SHAKOTN <shakotn@protonmail.com>
2022-09-21 19:04:49 +0300

    feat: add exc logging handler

--- badger_voter_sdk/json_logger.py
+++ badger_voter_sdk/json_logger.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 
 from pythonjsonlogger import jsonlogger
 
@@ -9,3 +10,10 @@ formatter = jsonlogger.JsonFormatter()
 log_handler.setFormatter(formatter)
 logger.addHandler(log_handler)
 logger.setLevel(logging.INFO)
+
+
+def exception_logging(exctype, value, tb) -> None:
+    logger.error(f"{str(exctype)} Exception", extra={
+        'exctype': str(exctype),
+        'error': ''.join(traceback.format_exception(exctype, value, tb))
+    })
